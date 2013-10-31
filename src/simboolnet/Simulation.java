de453665b145909cc7625b63fe1b15583878a67e
Lina Zhao <lina.oahz@gmail.com>
2013-10-31 17:42:45 +0800

    update

--- src/simboolnet/Simulation.java
+++ src/simboolnet/Simulation.java
@@ -1,9 +1,16 @@
 package simboolnet;
 
-public class Simulation {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import java.util.HashMap;
+import java.util.Map;
+
+public class Simulation {
+	
+
+	public static void main(String[] args) throws Exception {
+		ProcessingGraph processing = new ProcessingGraph();
+		Map <Vertice, Double> nodeSignal = new HashMap<Vertice, Double>();
+		
 
 	}
 
